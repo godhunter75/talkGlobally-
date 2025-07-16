37872bd1d8d523072dd2e69e9176b34b5dc1bcde
godhunter75 <tripathy1209@gmail.com>
2025-07-16 11:03:11 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
